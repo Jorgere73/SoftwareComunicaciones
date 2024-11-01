34f81bbf4aaa467f1e5970da087cfb6be515e60b
100472783 <100472783@alumnos.uc3m.es>
2024-11-01 15:09:12 +0100

    Borrado main en Initial.java

--- src/view/Initial.java
+++ src/view/Initial.java
@@ -20,25 +20,6 @@ public class Initial extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GlobalInstances.init = new Initial();
-					GlobalInstances.login = new Login();
-					GlobalInstances.registro = new Registro();
-					GlobalInstances.init.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
 	/**
 	 * Create the frame.
 	 */
--- src/view/Main.java
+++ src/view/Main.java
@@ -2,5 +2,11 @@ package view;
 
 public class Main 
 {
-	
+	public static void main(String[] args)
+	{
+		GlobalInstances.init = new Initial();
+		GlobalInstances.login = new Login();
+		GlobalInstances.registro = new Registro();
+		GlobalInstances.init.setVisible(true);
+	}
 }
